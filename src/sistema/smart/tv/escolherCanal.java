6c2b8f729ca231d128851d50879d49f0ac0600f3
MATHEUS HENRIQUE DO AMARAL PAULINO <theush933@gmail.com>
2023-03-08 16:23:06 -0300

    third commit with Intellij

---
+++ src/sistema/smart/tv/escolherCanal.java
@@ -0,0 +1,8 @@
+package sistema.smart.tv;
+
+public class escolherCanal {
+    public static void main(String[] args) {
+        smartTv smartTv = new smartTv();
+        smartTv.novoCanal(15);
+    }
+}
